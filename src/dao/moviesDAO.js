dbe3688e7eee3168d7307fee1abf97b502cf3007
Salatiel Osorno <salatiel.linares@gmail.com>
2019-07-03 17:49:55 -0500

    Chapter 4 Resiliency.

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -355,7 +355,7 @@ export default class MoviesDAO {
       // TODO Ticket: Error Handling
       // Catch the InvalidId error by string matching, and then handle it.
       console.error(`Something went wrong in getMovieByID: ${e}`)
-      throw e
+      return null //throw e
     }
   }
 }
